56907a8ef92819a22e8965bde4ef222c4b50218a
Mark-Oussoren <mark_oussoren@berkeley.edu>
2022-06-12 15:08:29 -0400

    adding ride

--- usaco-programs/ride.cpp
+++ usaco-programs/ride.cpp
@@ -4,8 +4,30 @@ LANG: C++
 TASK: ride
 */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
 int main() {
-    
+    ofstream fout ("ride.out");
+    ifstream fin ("ride.in");
+    string comet, group;
+    fin >> comet >> group;
+    // numerize the characters of the comet and group strs
+    int comet_num = 1, group_num = 1;
+    for (char & c : comet) {
+        comet_num *= int(c);
+    }
+    for (char & c : group) {
+        group_num *= int(c);
+    }
+    comet_num = comet_num % 47;
+    group_num = group_num % 47;
+    if (comet_num == group_num) {
+        fout << "GO" << endl
+    } else {
+        fout << "STAY" << endl
+    }
 }
